117e3943523fdb9bd651c6c15d952b923069d36c
KrasimirHristov <krasimir_2001@abv.bg>
2020-10-15 06:19:37 +0000

    Yeah

---
+++ src/exercise3/NamePrinter.java
@@ -0,0 +1,16 @@
+package exercise3;
+
+import java.util.Scanner;
+
+public class NamePrinter{
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String word = input.next();
+        for ( int i=0; i<word.length(); i++)
+        {
+          char letter= word.charAt(i);
+          System.out.println("* " + letter + " *");
+        }
+    }
+}
